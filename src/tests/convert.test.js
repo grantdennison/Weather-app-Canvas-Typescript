44fcad02ee45cb9cba9bf2f920cb0e40a1b04e87
Grant Dennison <grantdennison@yahoo.co.uk>
2022-11-20 20:02:31 +0000

    test

--- src/tests/convert.test.js
+++ src/tests/convert.test.js
@@ -5,3 +5,6 @@ import { expect, jest, test } from "@jest/globals";
 test(`Add 1+ 2 to equal 3`, () => {
   expect(sum(1, 2)).toBe(3);
 });
+test(`Add 1+ 2 to equal 3`, () => {
+  expect(sub(1, 2)).toBe(3);
+});
