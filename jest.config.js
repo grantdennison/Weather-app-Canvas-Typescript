405aac1297905bd7baa2ad1ae244d243abe571cd
Grant Dennison <grantdennison@yahoo.co.uk>
2022-11-20 11:26:33 +0000

    default

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node"
-};
-
-// function change Degrees C to F and distance km/h to mph
-
-// updateText();
